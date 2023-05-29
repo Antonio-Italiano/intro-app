cd0998565b6d4fff2d978ce58e031f12e4a2d77f
Antonio-Italiano <antoitalia221@gmail.com>
2023-05-29 14:35:46 +0200

    React hooks. useState e useEffect.

--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -1,6 +1,20 @@
+import {useEffect, useState} from 'react';
+
 const Clock = ({country, timezone}) => {
     const t = Date.now() + 3600 * timezone * 1000;
-    const date = new Date(t);
+    const dateIni = new Date(t);
+
+    const [date, setDate] = useState(dateIni);
+
+    useEffect(() => {
+        const intervall = setInterval(() => {
+            const time = date.getTime() + 1000;
+            setDate(new Date(time));
+        }, 1000);
+        return () => {
+            clearInterval(intervall);
+        }
+    }, [date])
     return (
     <h2>
         Today in {country} is { date.toLocaleDateString() + ' ' + date.toLocaleTimeString()}
